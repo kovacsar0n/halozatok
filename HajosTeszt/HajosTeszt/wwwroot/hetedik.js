74a87281b491a7905d04e921396363155756f892
kovacs.aron <kovacs.aron@DESKTOP-KEHCNHC>
2021-03-27 22:51:22 +0100

    JSON (7.gyak)

---
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -0,0 +1,47 @@
+﻿var kérdések;
+var kérdésSorszám = 0;
+
+function letöltés() {
+    fetch("/questions.json")
+        .then(response => response.json())
+        .then(d => letöltésBefejeződött(d));
+}
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    kérdések = d;
+    kérdésMegjelenítés(0);
+}
+var kérdésMegjelenítés = function (kérdésSzáma) {
+    let kérdés_szöveg = document.getElementById("kérdés_szöveg");
+    let kép = document.getElementById("kép1");
+    let válasz1 = document.getElementById("válasz1");
+    let válasz2 = document.getElementById("válasz2");
+    let válasz3 = document.getElementById("válasz3");
+
+    kérdés_szöveg.innerHTML = kérdések[kérdésSzáma].questionText
+    kép.src = "https://szoft1.comeback.hu/hajo/" + kérdések[kérdésSzáma].image
+    válasz1.innerText = kérdések[kérdésSzáma].answer1
+    válasz2.innerText = kérdések[kérdésSzáma].answer2
+    válasz3.innerText = kérdések[kérdésSzáma].answer3
+}
+
+
+
+window.onload = () => {
+    letöltés();
+
+
+    document.getElementById("vissza").onclick = () => {
+        kérdésSorszám--;
+        if (kérdésSorszám == -1) {
+            kérdésMegjelenítés(kérdésSorszám[2]);
+        }
+    }
+    document.getElementById("előre").onclick = () => {
+        kérdésSorszám++;
+        if (kérdésSorszám == 3) {
+            kérdésMegjelenítés(kérdésSorszám[0]);
+        }
+    }
+}
